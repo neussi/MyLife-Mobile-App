f90eee8bdee048a5d745557cfd40d990600f909e
neussi <neussi344@gmail.com>
2026-02-19 19:37:06 +0100

    new deploy

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib import messages
 from django.db.models import Q
 from django.http import JsonResponse
 from .models import Contact
